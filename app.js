51f05c89f0c772dd406e1aac59795b7ec239c629
MrBuggysan <MrBuggySan@users.noreply.github.com>
2016-10-25 16:42:31 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
-+**TODO:**
- +- Run the node app with the chaincode service and blockchain service 
- +- Go through registration process 
- +- Go through enrollment process 
- +- invoke chaincode transactions (put in values) ie. list your favourite food!
+/*eslint-env node*/
+ 
+ //----------------------
+ // node.js starter application for Bluemix
+ //---------------
+ 
+ // This application uses express as its web server
+ // for more info, see: http://expressjs.com
+ var express = require('express');
+ 
+ // cfenv provides access to your Cloud Foundry environment
+ // for more info, see: https://www.npmjs.com/package/cfenv
+ var cfenv = require('cfenv');
+ 
+ //hyperledger SDK
+ var hfc = require('hfc');
+ 
+ // create a new express server
+ var app = express();
+ 
+ // serve the files out of ./public as our main files
+ app.use(express.static(__dirname + '/public'));
+ 
+ // get the app environment from Cloud Foundry
+ var appEnv = cfenv.getAppEnv();
+ 
+ // start server on the specified port and binding host
+ app.listen(appEnv.port, '0.0.0.0', function() {
+   // print a message when the server starts listening
+   console.log("server starting on " + appEnv.url);
+ });
