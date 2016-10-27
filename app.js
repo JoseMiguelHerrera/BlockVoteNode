33ebd4cc58b5f46c1a4c91f2d6625a18f05330b1
Andrei Buiza <andreibuiza@gmail.com>
2016-10-27 17:47:09 -0600

    Adding more skeleton code. The code fails at enrolling admin

--- app.js
+++ app.js
@@ -10,14 +10,23 @@ var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+// var cfenv = require('cfenv');
 
 //hyperledger SDK
 var hfc = require('hfc');
 //Access directories 
 var fs = require('fs');
+const https = require('https');
 
-
+// Creating an environment variable for ciphersuites
+process.env['GRPC_SSL_CIPHER_SUITES'] = 'ECDHE-RSA-AES128-GCM-SHA256:' +
+    'ECDHE-RSA-AES128-SHA256:' +
+    'ECDHE-RSA-AES256-SHA384:' +
+    'ECDHE-RSA-AES256-GCM-SHA384:' +
+    'ECDHE-ECDSA-AES128-GCM-SHA256:' +
+    'ECDHE-ECDSA-AES128-SHA256:' +
+    'ECDHE-ECDSA-AES256-SHA384:' +
+    'ECDHE-ECDSA-AES256-GCM-SHA384';
 //*******************************HFC SDK SETUP START ******************************
 
 // Create a client blockchin.
@@ -39,36 +48,99 @@ try {
 var peers = network.credentials.peers;
 var users = network.credentials.users;
 
-var network_id = Object.keys(network.credentials.ca);
-var uuid = network_id[0].substring(0, 8);
-//keyValStore is local 
-chain.setKeyValStore(hfc.newFileKeyValStore(__dirname + '/keyValStore-' + uuid));
-
-// Set the URL for membership services
-var ca_url = "grpcs://" + network.credentials.ca[network_id].discovery_host + ":" + network.credentials.ca[network_id].discovery_port;
-chain.setMemberServicesUrl(ca_url);
-
-
-// Adding all the peers to blockchain
-// this adds high availability for the client
-for (var i = 0; i < peers.length; i++) {
-    chain.addPeer("grpcs://" + peers[i].discovery_host + ":" + peers[i].discovery_port);
-}
-
-console.log("\n\n------------- peers and caserver information: -------------");
-console.log(chain.getPeers());
-console.log(chain.getMemberServices());
-console.log('-----------------------------------------------------------\n\n');
-
-//enroll the admin 
-chain.enroll(users[0].username, users[0].secret, function(err, admin) {
-    if (err) throw Error("\nERROR: failed to enroll admin : %s", err);
-    // Set this user as the chain's registrar which is authorized to register other users.
-    chain.setRegistrar(users[0].username);
+//Download the certificates from Bluemix
+var certFile = 'certificate.pem';
+var certUrl = network.credentials.cert;
+fs.access(certFile, function(err) {
+    if (!err) {
+        console.log("\nDeleting existing certificate ", certFile);
+        fs.unlinkSync(certFile);
+    }
+    
+    downloadCertificate();
 });
 
+function downloadCertificate() {
+    var file = fs.createWriteStream(certFile);
+    var data = '';
+    https.get(certUrl, function(res) {
+        console.log('\nDownloading %s from %s', certFile, certUrl);
+        if (res.statusCode !== 200) {
+            console.log('\nDownload certificate failed, error code = %d', certFile, res.statusCode);
+            process.exit();
+        }
+        res.on('data', function(d) {
+            data += d;
+        });
+        // event received when certificate download is completed
+        res.on('end', function() {
+            if (process.platform != "win32") {
+                data += '\n';
+            }
+            fs.writeFileSync(certFile, data);
+            copyCertificate();
+        });
+    }).on('error', function(e) {
+        console.error(e);
+        process.exit();
+    });
+}
+
+function copyCertificate() {
+    //fs.createReadStream('certificate.pem').pipe(fs.createWriteStream(ccPath+'/certificate.pem'));
+    fs.writeFileSync(ccPath + '/certificate.pem', fs.readFileSync(__dirname + '/certificate.pem'));
+
+    setTimeout(function() {
+        enrollAdmin();
+    }, 1000);
+}
+
+
+var network_id = Object.keys(network.credentials.ca);
+var uuid = network_id[0].substring(0, 8);
+
+//keyValStore is local 
+
+chain.setKeyValStore(hfc.newFileKeyValStore(__dirname + '/keyValStore-' + uuid));
+
+
+function enrollAdmin() {
+
+    // Set the URL for membership services
+    var ca_url = "grpcs://" + network.credentials.ca[network_id].discovery_host + ":" + network.credentials.ca[network_id].discovery_port;
+    var cert = fs.readFileSync(certFile);
+    chain.setMemberServicesUrl(ca_url, {
+        pem: cert
+    });
+
+    // Adding all the peers to blockchain
+    // this adds high availability for the client
+    for (var i = 0; i < peers.length; i++) {
+        chain.addPeer("grpcs://" + peers[i].discovery_host + ":" + peers[i].discovery_port, {
+            pem: cert
+        });
+    }
+
+    console.log("\n\n------------- peers and caserver information: -------------");
+    console.log(chain.getPeers());
+    console.log(chain.getMemberServices());
+    console.log('-----------------------------------------------------------\n\n');
+
+    console.log("Enrolling the admin... ");
+    //enroll the admin 
+    chain.enroll(users[0].username, users[0].secret, function(err, admin) {
+        if (err) throw Error("\nERROR: failed to enroll admin : %s", err);
+        // Set this user as the chain's registrar which is authorized to register other users.
+        
+        chain.setRegistrar(users[0].username);
+    });
+}
 //admin deploys the chaincode 
-function deploy(){
+function deploy() {
+
+}
+
+function testProgram() {
 
 }
 
@@ -83,19 +155,24 @@ app.use(express.static(__dirname + '/public'));
 
 
 
-//Setup the APIs for communication between front end and back end 
+// app.listen(port, hostname, function() {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
 
+//Setup the APIs for communication between front end and back end 
 
 //functions to interface with IBM Blockchain
 function registerNewUser() {
-	//admin registers the new user
+    //admin registers the new user
 
 }
 
 function enrollUser() {
-	//admin enrolls the user
+    //admin enrolls the user
 }
 
+
+//set the vote 
 function invoke() {
 
 }
@@ -106,11 +183,12 @@ function query() {
 
 //*******************************WEB APP SERVICE DONE ******************************
 
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-    // print a message when the server starts listening
-    console.log("server starting on " + appEnv.url);
-});
+// // get the app environment from Cloud Foundry
+// var appEnv = cfenv.getAppEnv();
+
+// // start server on the specified port and binding host
+// app.listen(appEnv.port, '0.0.0.0', function() {
+//     // print a message when the server starts listening
+//     console.log("server starting on " + appEnv.url);
+// });
