7db3d8f329a492a172c012fdac16e4f82b16f779
Andrei Buiza <andreibuiza@gmail.com>
2016-10-28 16:32:40 -0600

    Admin can now be enrolled, Admin can now deploy chaincode to IBM Blockchain

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-**TODO:**
-- Run the node app with the chaincode service and blockchain service 
-- Go through registration process 
-- Go through enrollment process 
-- invoke chaincode transactions (put in values) ie. list your favourite food! 
+**Overview**
+=============
+- Create a referendum
+- Register and enroll the participant 
+- Enter each participant's vote on the Blockchain
+- Count the votes in the Blockchain
+- Present the results of the referendum 
+- First phase: automatic registration without registrar validity
 
+**TODO/Milestones**
+- [ ] Register and enroll voters
+- [ ] Serve results back to voters  
 
 **Resource**
- - github.com/hyperledger/fabric/sdk/node/lib/hfc.js 
+ - Read this to see the methods in HFC: github.com/hyperledger/fabric/sdk/node/lib/hfc.js
+
+**Notes**
+- For now, the app will work locally and connect to an IBM Blockchain Service on Bluemix
+- Copy your spefic service credentials to ServiceCredentials.json  
--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 /*eslint-env node*/
 
-//----------------------
-// node.js starter application for Bluemix
-//---------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
 var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
@@ -18,6 +12,9 @@ var hfc = require('hfc');
 var fs = require('fs');
 const https = require('https');
 
+var USE_BLOCKVOTE_CC = false;
+var DEV_MODE = false;
+//Andrei: What are these? 
 // Creating an environment variable for ciphersuites
 process.env['GRPC_SSL_CIPHER_SUITES'] = 'ECDHE-RSA-AES128-GCM-SHA256:' +
     'ECDHE-RSA-AES128-SHA256:' +
@@ -33,7 +30,14 @@ process.env['GRPC_SSL_CIPHER_SUITES'] = 'ECDHE-RSA-AES128-GCM-SHA256:' +
 var chain = hfc.newChain("BallotChain")
 
 //Set the default chaincode path 
-var ccPath = process.env["GOPATH"] + "/src/BlockVoteChainCode";
+var ccPath = "";
+if (USE_BLOCKVOTE_CC) {
+    ccPath = process.env["GOPATH"] + "/src/BlockVoteChainCode/start";
+} else {
+    ccPath = process.env["GOPATH"] + "/src/chaincode_example02";
+}
+
+
 console.log("The chaincode is supposed to bet at:" + ccPath);
 
 // Read and process the credentials.json
@@ -48,6 +52,7 @@ try {
 var peers = network.credentials.peers;
 var users = network.credentials.users;
 
+//Andrei: what is this for?
 //Download the certificates from Bluemix
 var certFile = 'certificate.pem';
 var certUrl = network.credentials.cert;
@@ -56,7 +61,7 @@ fs.access(certFile, function(err) {
         console.log("\nDeleting existing certificate ", certFile);
         fs.unlinkSync(certFile);
     }
-    
+
     downloadCertificate();
 });
 
@@ -85,7 +90,7 @@ function downloadCertificate() {
         process.exit();
     });
 }
-
+//copy the certificate.pem over to the chaincode folder 
 function copyCertificate() {
     //fs.createReadStream('certificate.pem').pipe(fs.createWriteStream(ccPath+'/certificate.pem'));
     fs.writeFileSync(ccPath + '/certificate.pem', fs.readFileSync(__dirname + '/certificate.pem'));
@@ -100,9 +105,9 @@ var network_id = Object.keys(network.credentials.ca);
 var uuid = network_id[0].substring(0, 8);
 
 //keyValStore is local 
-
 chain.setKeyValStore(hfc.newFileKeyValStore(__dirname + '/keyValStore-' + uuid));
 
+var admin;
 
 function enrollAdmin() {
 
@@ -126,17 +131,60 @@ function enrollAdmin() {
     console.log(chain.getMemberServices());
     console.log('-----------------------------------------------------------\n\n');
 
+    if (DEV_MODE) {
+        chain.setDevMode(true);
+        console.log("The chain is set to development mode");
+        //Deploy will not take long as the chain should already be running
+        chain.setDeployWaitTime(10);
+    } else {
+        chain.setDeployWaitTime(120);
+    }
+
+
     console.log("Enrolling the admin... ");
     //enroll the admin 
-    chain.enroll(users[0].username, users[0].secret, function(err, admin) {
+    chain.enroll(users[0].username, users[0].secret, function(err, user) {
         if (err) throw Error("\nERROR: failed to enroll admin : %s", err);
         // Set this user as the chain's registrar which is authorized to register other users.
-        
         chain.setRegistrar(users[0].username);
+        console.log("Admin is now enrolled.")
+
+
+        admin = user;
+        deploy();
     });
 }
 //admin deploys the chaincode 
 function deploy() {
+
+    console.log("Deploying chaincode ...");
+    // Construct the deploy request
+    var deployRequest = {
+        // Function to trigger
+        fcn: "init",
+        // Arguments to the initializing function
+        args: ["a", "100", "b", "200"]
+    };
+    if (USE_BLOCKVOTE_CC) {
+        deployRequest.chaincodePath = "BlockVoteChainCode/start";
+    } else {
+        deployRequest.chaincodePath = "chaincode_example02";
+    }
+
+    // Issue the deploy request and listen for events
+    var tx = admin.deploy(deployRequest);
+    tx.on('complete', function(results) {
+        // Deploy request completed successfully
+        console.log("deploy complete; results: %j", results);
+        // Set the testChaincodeID for subsequent tests
+        chaincodeID = results.chaincodeID;
+        console.log("Chaincode ID: " + chaincodeID);
+    });
+    tx.on('error', function(error) {
+        console.log("Failed to deploy chaincode: request=%j, error=%k", deployRequest, error);
+        process.exit(1);
+    });
+
 
 }
 
